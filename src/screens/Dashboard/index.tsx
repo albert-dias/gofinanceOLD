686f7b6954264193e20c5714a8dcd250296141be
Albert Dias <albert.uft@gmail.com>
2021-06-05 08:47:50 -0300

    gofinances init styled components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Container } from './styles';
+
+
+export function Dashboard(){
+    return (
+        <Container>
+            <Text>goFinances</Text>
+        </Container>
+    )
+}
