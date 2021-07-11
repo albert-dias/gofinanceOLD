b8b8d8e8aea563e3c9799c024ca2805b9ee0f239
Albert Dias <albert.uft@gmail.com>
2021-07-11 10:01:23 -0300

    created and style tab router

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
-import AppLoading from 'expo-app-loading'
+import AppLoading from 'expo-app-loading';
+
+import { NavigationContainer } from '@react-navigation/native'
+
 import {
   useFonts,
   Poppins_400Regular,
@@ -9,8 +13,8 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
+
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +29,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
